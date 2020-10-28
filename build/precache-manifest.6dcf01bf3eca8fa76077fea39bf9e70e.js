d15952f83b6d4af524519176fc63c94bee3b7556
sc0vu <alk03073135@gmail.com>
2020-10-28 17:20:21 +0800

    Add latest builded assets

---
+++ build/precache-manifest.6dcf01bf3eca8fa76077fea39bf9e70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd15c6c65ef5b2f6913986844c05070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06df75fde75de7e6c80f",
+    "url": "/static/css/main.800e36df.chunk.css"
+  },
+  {
+    "revision": "2f7493bef7fc9fd224e1",
+    "url": "/static/js/2.54a5d71a.chunk.js"
+  },
+  {
+    "revision": "a00800a03760bfecf020330e10aebc24",
+    "url": "/static/js/2.54a5d71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06df75fde75de7e6c80f",
+    "url": "/static/js/main.ffd5db4c.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
